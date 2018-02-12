8de9de8bee3af1475596d60022774fbd3b1fa3a7
Diego Cáceres <dado90caceres@gmail.com>
2018-02-12 07:50:05 -0300

    Added two lists of products

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -2,21 +2,9 @@ import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput, Button, Alert, FlatList } from "react-native";
 import ProductItem from './ProductItem';
 
-const products = [
-  {id: 1, name: 'Product 1', price: '$100'},
-  {id: 2, name: 'Product 2', price: '$315'},
-  {id: 3, name: 'Product 3', price: '$115'},
-  {id: 4, name: 'Product 4', price: '$150'},
-  {id: 5, name: 'Product 5', price: '$225'}
-];
-
 export default class ProductsList extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      products: products//[],
-    }
   }
 
   renderRow = (item, index) => {
@@ -32,10 +20,10 @@ export default class ProductsList extends React.Component {
   render() {
     return (
       <View style={styles.container}> 
-        {this.state.products.length > 0 && <Text style={{textAlign: 'center'}}> Results: </Text>}
+        {this.props.products.length > 0 && <Text style={{textAlign: 'center'}}> {this.props.listTitle}: </Text>}
         <FlatList
           style={styles.listContainer}
-          data={this.state.products}
+          data={this.props.products}
           renderItem={({ item, index }) => this.renderRow(item, index)}
           keyExtractor={this._keyExtractor}
         />
--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 import ProductsList from '../components/ProductsList';
 
+
+const products = [
+  {id: 1, name: 'Product 1', price: '$100'},
+  {id: 2, name: 'Product 2', price: '$315'},
+  {id: 3, name: 'Product 3', price: '$115'},
+  {id: 4, name: 'Product 4', price: '$150'},
+  {id: 5, name: 'Product 5', price: '$225'}
+];
+
+const wishProducts = [
+  {id: 1, name: 'Wishlist Product 1', price: '$100'},
+  {id: 2, name: 'Wishlist Product 2', price: '$315'},
+  {id: 3, name: 'Wishlist Product 3', price: '$115'},
+  {id: 4, name: 'Wishlist Product 4', price: '$150'},
+  {id: 5, name: 'Wishlist Product 5', price: '$225'}
+];
+
 export default class ProductsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: products, //[],
+      wishListProducts: wishProducts, //[],
+      showWishList: false
+    }
+
+  }
+  toggleShow = () => {
+    this.setState({ showWishList: !this.state.showWishList });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <ProductsList />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.switchButton}>
+            <Button  
+              onPress={this.toggleShow}
+              title={this.state.showWishList ? "Show Products" : "Show Wishlist"} />
+          </View>
+        </View>
+        <View style={{ flex: 1}}>
+          <ProductsList 
+            products={!this.state.showWishList ? this.state.products : this.state.wishListProducts}
+            listTitle={!this.state.showWishList ? 'Products' : 'WishList' }
+          />
+        </View>
       </View>
     );
   }
@@ -18,5 +60,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  switchButton: {
+    alignSelf: 'flex-end',
+    width: 100,
+    borderWidth: 1,
+    borderColor: 'grey'
   },
 });
